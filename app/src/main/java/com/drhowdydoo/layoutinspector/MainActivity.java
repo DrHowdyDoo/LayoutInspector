a222e5092189e398dd8f3417c794cde9646f071f
DrHowdyDoo <55204060+DrHowdyDoo@users.noreply.github.com>
2024-03-16 16:18:57 +0530

    add main activity screen layout

--- app/src/main/java/com/drhowdydoo/layoutinspector/MainActivity.java
+++ app/src/main/java/com/drhowdydoo/layoutinspector/MainActivity.java
@@ -4,7 +4,11 @@ import android.content.ComponentName;
 import android.content.Intent;
 import android.os.Bundle;
 import android.provider.Settings;
+import android.text.Spannable;
+import android.text.SpannableStringBuilder;
 import android.util.Log;
+import android.view.View;
+import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -17,23 +21,32 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         DynamicColors.applyToActivityIfAvailable(this);
         setContentView(R.layout.activity_main);
+        TextView tvOpenAssistSetting = findViewById(R.id.tvOpenAssistSetting);
+        TextView tvInfoStep2 = findViewById(R.id.info_step_2);
+        TextView tvInfoStep3 = findViewById(R.id.info_step_3);
+        TextView tvInfoStep4 = findViewById(R.id.info_step_4);
+        setBoldText(tvInfoStep2,7,tvInfoStep2.getText().length());
+        setBoldText(tvInfoStep3,7,27);
+        setBoldText(tvInfoStep4,7,tvInfoStep4.getText().length() - 10);
+        tvOpenAssistSetting.setOnClickListener(v -> {
+            startActivity(new Intent(Settings.ACTION_VOICE_INPUT_SETTINGS));
+        });
 
-        String assistant=
-                Settings.Secure.getString(getContentResolver(),
-                        "voice_interaction_service");
-
+        String assistant= Settings.Secure.getString(getContentResolver(), "voice_interaction_service");
         boolean areWeGood=false;
-
         if (assistant!=null) {
             ComponentName cn=ComponentName.unflattenFromString(assistant);
-
             if (cn != null && cn.getPackageName().equals(getPackageName())) {
                 areWeGood = true;
             }
         }
 
-        if (!areWeGood) {
-            startActivity(new Intent(Settings.ACTION_VOICE_INPUT_SETTINGS));
-        }
+
+    }
+
+    private void setBoldText(TextView tv, int start, int end){
+        SpannableStringBuilder str = new SpannableStringBuilder(tv.getText());
+        str.setSpan(new android.text.style.StyleSpan(android.graphics.Typeface.BOLD), start, end, Spannable.SPAN_EXCLUSIVE_EXCLUSIVE);
+        tv.setText(str);
     }
 }
