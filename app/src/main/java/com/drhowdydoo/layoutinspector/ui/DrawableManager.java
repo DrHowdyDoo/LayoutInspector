e64e187ae2fbc81bdf5572e48696f261c93dca4b
DrHowdyDoo <55204060+DrHowdyDoo@users.noreply.github.com>
2024-03-17 23:37:33 +0530

    add DrawableManager class

---
+++ app/src/main/java/com/drhowdydoo/layoutinspector/ui/DrawableManager.java
@@ -0,0 +1,53 @@
+package com.drhowdydoo.layoutinspector.ui;
+
+import com.drhowdydoo.layoutinspector.R;
+
+public class DrawableManager {
+
+    public static int getDrawableForView(String viewName) {
+        switch (viewName) {
+            case "TextView":
+                return R.drawable.text_view_icon;
+            case "ImageView":
+                return R.drawable.image_view_icon;
+            case "ImageButton":
+                return R.drawable.image_button_icon;
+            case "LinearLayout":
+                return R.drawable.linear_layout_icon;
+            case "RelativeLayout":
+                return R.drawable.relative_layout_icon;
+            case "FrameLayout":
+                return R.drawable.frame_layout_icon;
+            case "ConstraintLayout":
+                return R.drawable.constraint_layout_icon;
+            case "RecyclerView":
+                return R.drawable.recycler_view_icon;
+            case "RadioButton":
+                return R.drawable.radio_btn_icon;
+            case "SearchView":
+                return R.drawable.search_view_icon;
+            case "ViewGroup":
+                return R.drawable.viewgroup_icon;
+            case "Slider":
+                return R.drawable.slider_icon;
+            case "ScrollView":
+                return R.drawable.scroll_view_icon;
+            case "HorizontalScrollView":
+                return R.drawable.horizontal_scroll_view_icon;
+            case "NestedScrollView":
+                return R.drawable.nested_scroll_view_icon;
+            case "CheckBox":
+                return R.drawable.checkbox_icon;
+            case "MaterialCheckBox":
+            case "AppCompatCheckBox":
+            case "MaterialSwitch":
+            case "SwitchMaterial":
+            case "SwitchCompat":
+            case "Switch":
+                return R.drawable.switch_icon;
+            default:
+                return R.drawable.view_icon;
+        }
+    }
+
+}
--- app/src/main/java/com/drhowdydoo/layoutinspector/ui/HierarchyViewHolder.java
+++ app/src/main/java/com/drhowdydoo/layoutinspector/ui/HierarchyViewHolder.java
@@ -24,52 +24,7 @@ public class HierarchyViewHolder extends TreeViewHolder {
         super.bindTreeNode(node);
         // Here you can bind your node and check if it selected or not
         tvNode.setText(node.getValue().toString());
-        switch (node.getValue().toString()){
-            case "TextView" : imgIcon.setBackgroundResource(R.drawable.text_view_icon);
-            break;
-            case "ImageView" : imgIcon.setBackgroundResource(R.drawable.image_view_icon);
-            break;
-            case "ImageButton" : imgIcon.setBackgroundResource(R.drawable.image_button_icon);
-            break;
-            case "LinearLayout" : imgIcon.setBackgroundResource(R.drawable.linear_layout_icon);
-            break;
-            case "RelativeLayout":imgIcon.setBackgroundResource(R.drawable.relative_layout_icon);
-            break;
-            case "FrameLayout": imgIcon.setBackgroundResource(R.drawable.frame_layout_icon);
-            break;
-            case "ConstraintLayout" : imgIcon.setBackgroundResource(R.drawable.constraint_layout_icon);
-            break;
-            case "RecyclerView" : imgIcon.setBackgroundResource(R.drawable.recycler_view_icon);
-            break;
-            case "RadioButton" : imgIcon.setBackgroundResource(R.drawable.radio_btn_icon);
-            break;
-            case "SearchView" : imgIcon.setBackgroundResource(R.drawable.search_view_icon);
-            break;
-            case "MaterialSwitch" :
-            case "SwitchMaterial":
-            case "SwitchCompat":
-            case "Switch" : imgIcon.setBackgroundResource(R.drawable.switch_icon);
-            break;
-            case "ViewGroup" : imgIcon.setBackgroundResource(R.drawable.viewgroup_icon);
-            break;
-            case "Slider" : imgIcon.setBackgroundResource(R.drawable.slider_icon);
-            break;
-            case "ScrollView" : imgIcon.setBackgroundResource(R.drawable.scroll_view_icon);
-            break;
-            case "HorizontalScrollView" : imgIcon.setBackgroundResource(R.drawable.horizontal_scroll_view_icon);
-            break;
-            case "NestedScrollView" : imgIcon.setBackgroundResource(R.drawable.nested_scroll_view_icon);
-            break;
-            case "MaterialCheckBox" :
-            case "AppCompatCheckBox" :
-            case "CheckBox" : imgIcon.setBackgroundResource(R.drawable.checkbox_icon);
-            break;
-
-
-
-            default: imgIcon.setBackgroundResource(R.drawable.view_icon);
-
-        }
+        imgIcon.setBackgroundResource(DrawableManager.getDrawableForView(node.getValue().toString()));
         if (node.isExpanded()) {
             imgArrow.setBackgroundResource(R.drawable.rounded_arrow_drop_down_24);
         } else imgArrow.setBackgroundResource(R.drawable.rounded_arrow_right_24);
