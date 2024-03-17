208ca0622d6f2721a89e9d79e6d2ee5271a0c037
DrHowdyDoo <55204060+DrHowdyDoo@users.noreply.github.com>
2024-03-17 23:36:01 +0530

    add utility functions

--- app/src/main/java/com/drhowdydoo/layoutinspector/util/Utils.java
+++ app/src/main/java/com/drhowdydoo/layoutinspector/util/Utils.java
@@ -1,19 +1,26 @@
 package com.drhowdydoo.layoutinspector.util;
 
 import android.app.assist.AssistStructure;
+import android.content.Context;
+import android.graphics.Rect;
+import android.util.DisplayMetrics;
 
 import com.amrdeveloper.treeview.TreeNode;
 import com.drhowdydoo.layoutinspector.R;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 public class Utils {
     private static final List<TreeNode> componentTree = new ArrayList<>();
+    public static final Map<AssistStructure.ViewNode, Rect> viewNodeRectMap = new HashMap<>();
 
     public static List<TreeNode> displayViewHierarchy(AssistStructure assistStructure) {
         int windowNodeCount = assistStructure.getWindowNodeCount();
         componentTree.clear();
+        viewNodeRectMap.clear();
         for (int i = 0; i < windowNodeCount; i++) {
             AssistStructure.WindowNode windowNode = assistStructure.getWindowNodeAt(i);
             displayViewHierarchyRecursive(windowNode.getRootViewNode(), 0,null);
@@ -25,6 +32,7 @@ public class Utils {
         TreeNode root = new TreeNode(getLastSegmentOfClass(viewNode.getClassName()),R.layout.hierarchy_tree_node);
         if (parent != null) parent.addChild(root);
         componentTree.add(root);
+        viewNodeRectMap.put(viewNode,new Rect(viewNode.getLeft(),viewNode.getTop(),viewNode.getLeft() + viewNode.getWidth(), viewNode.getTop() + viewNode.getHeight()));
 
         if (viewNode.getChildCount() > 0) {
             for (int i = 0; i < viewNode.getChildCount(); i++) {
@@ -33,7 +41,7 @@ public class Utils {
         }
     }
 
-    private static String getLastSegmentOfClass(String className) {
+    public static String getLastSegmentOfClass(String className) {
         if (className == null || className.isEmpty()) {
             return "";
         }
@@ -41,5 +49,19 @@ public class Utils {
         return segments[segments.length - 1];
     }
 
+    public static int pxToDp(Context context, int px) {
+        DisplayMetrics displayMetrics = context.getResources().getDisplayMetrics();
+        return Math.round(px / displayMetrics.density);
+    }
+
+    public static int pxToSp(Context context, int px) {
+        DisplayMetrics displayMetrics = context.getResources().getDisplayMetrics();
+        float scaledDensity = displayMetrics.scaledDensity;
+        return Math.round(px / scaledDensity);
+    }
+
+    public static String intToHexString(int color) {
+        return String.format("#%06X", (0xFFFFFF & color));
+    }
 
 }
