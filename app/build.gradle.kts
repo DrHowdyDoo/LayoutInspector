eb0df285d5ade1aab9797745935febb644176a13
DrHowdyDoo <55204060+DrHowdyDoo@users.noreply.github.com>
2024-03-21 17:04:21 +0530

    add splashScreen

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -42,6 +42,7 @@ dependencies {
     implementation("com.google.android.material:material:1.12.0-alpha03")
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
     implementation("io.github.amrdeveloper:treeview:1.1.4")
+    implementation("androidx.core:core-splashscreen:1.0.1")
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
--- app/src/main/java/com/drhowdydoo/layoutinspector/MainActivity.java
+++ app/src/main/java/com/drhowdydoo/layoutinspector/MainActivity.java
@@ -11,6 +11,7 @@ import android.text.style.ForegroundColorSpan;
 import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.splashscreen.SplashScreen;
 
 import com.google.android.material.color.DynamicColors;
 import com.google.android.material.color.MaterialColors;
@@ -21,6 +22,7 @@ public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        SplashScreen splashScreen = SplashScreen.installSplashScreen(this);
         super.onCreate(savedInstanceState);
         DynamicColors.applyToActivityIfAvailable(this);
         setViewContent();
