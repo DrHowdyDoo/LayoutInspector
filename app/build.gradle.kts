0b518b111df306cb94dc9dd43fb83c6c4002d440
DrHowdyDoo <55204060+DrHowdyDoo@users.noreply.github.com>
2024-03-24 13:49:32 +0530

    remove QUERY_ALL_PACKAGES permission from manifest

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -10,7 +10,7 @@ android {
         applicationId = "com.drhowdydoo.layoutinspector"
         minSdk = 29
         targetSdk = 34
-        versionCode = 3
+        versionCode = 4
         versionName = "1.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
